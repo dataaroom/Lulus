28fe98c546d0c017fe559b9d916c600bb4395309
dataaroom <simon.shenjd@gmail.com>
2017-08-11 14:02:37 -0500

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,9 +1,10 @@
-list = [1,2,3,4]
-def donothing():
-    list = [6]
-    print (list)
+# python3
 
-n = 5
 
-print(donothing())
-print (list)
+
+def unitstarnd(value, unit,category)    # # 将输入单位转换成国际标准单位。
+#TODO: 
+    if unit in i.keys():
+            out = convert(i,unit,getattr(i,unit)[0],value)     #调用convert函数。
+            return out
+     
